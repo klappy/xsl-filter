4aab3df01beff3db795300fb7f3fdc40d68b1b97
Christopher Klapp <christopher@klapp.name>
2014-12-23 15:31:36 -0500

    added examples to test

--- example/example.rb
+++ example/example.rb
@@ -1,4 +1,6 @@
 require '../lib/xsl_filter.rb'
 
 paying = XslTemplate.new('./templates/paying-for-college-value-for-your-money.xsl')
+paying.test
 learning = XslTemplate.new('./templates/academic-life-distance-learning.xsl')
+learning.test
